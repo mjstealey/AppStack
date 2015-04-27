91720bfdd562945561db7b829b3c76b3cc982b52
Michael J. Stealey <Michael.J.Stealey@gmail.com>
2015-04-27 09:07:40 -0400

    add appstack-build.sh script

---
+++ appstack-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# appstack-build
+# Author: Michael Stealey <michael.j.stealey@gmail.com>
+
+APPSTACK_PATH=$1
+
+for f in $(ls -l ${1} | tr -s ' ' | grep "^d" | cut -d ' ' -f 9)
+do
+    echo "*** docker build -t ${f} . ***"
+    cd ${APPSTACK_PATH}/${f}
+    docker build -t ${f} .;
+done
